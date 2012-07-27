9883b26f609386677d6b87eb982de90f431daf41
Celine Quoy <cq215@caret.cam.ac.uk>
2012-07-27 17:28:42 +0100

    Quiz 27072012 2

--- devwidgets/quiz/javascript/quiz.js
+++ devwidgets/quiz/javascript/quiz.js
@@ -37,30 +37,45 @@ require(['jquery', 'sakai/sakai.api.core', 'jquery-ui'], function($, sakai) {
          * which mode the widget is in (settings or main), loads the necessary data
          * and shows the correct view.
          */
-        var $quizContainerAddNewQuestion = $('#quiz_container_add_newquestion');
-        var $quizContainerAddNewAnswerButton = $('#quiz_container_add_newanswer_button');
-        var $quizContainerRemoveLastAnswerButton = $('#quiz_container_remove_lastanswer_button');
-        var $quizContainerAddNewQuestionTable = $('#quiz_container_add_newquestion_table');
-        var quizContainerAddNewQuestionTable = '#quiz_container_add_newquestion_table';
-        var quizAddNewQuestionTableTemplate = 'quiz_add_newquestion_table_template';
-        var quizContainerAddNewQuestionQuestion = '#quiz_container_add_newquestion_question';
-        var quizContainerAddNewQuestionAnswer_0 = '#quiz_container_add_newquestion_answer_0';
-        var quizContainerAddNewQuestionAnswer_1 = '#quiz_container_add_newquestion_answer_1';
-        var $quizContainerAddNewQuestionToList = $('.quiz_container_add_newquestion_to_list');
-        var $quizContainerAddNewQuestionRaquoRight = $('#quiz_container_add_newquestion_raquo_right');
-        var quizContainerAddNewQuestionCorrectComment = $('#quiz_container_add_newquestion_correctcomment');
-        var quizContainerAddNewQuestionIncorrectComment = $('#quiz_container_add_newquestion_incorrectcomment');
-        var $quizContainerQuestionListUl = $('#quiz_container_questionlist_ul');
-        var quizContainerQuestionListTemplate = 'quiz_container_questionlist_template';
-        var $quizContainerQuestionToRemove = $('.quiz_container_question_to_remove');
-        var $quizContainerQuestionListShowDetails = $('#quiz_container_questionlist_show_details');
-        var quizContainerQuestionDetailsTemplate = '#quiz_container_questiondetails_template';
-        var buttonDisabled = "s3d-disabled";
+        
+        
         var answersNumber = 1;
         var questionsNumber = -1;
         var questionsList = [];
         var minAnswersNumber = 1;
         var questionCanBeSaved = false;
+        
+        //Node
+        var $quizNewQuestionForm = $('#quiz_newquestion_form');
+        var $quizNewQuestionAddButton = $('#quiz_newquestion_add_button');
+        var $quizNewQuestionRemoveButton = $('#quiz_newquestion_remove_button');
+        var $quizNewQuestionTable = $('#quiz_newquestion_table');
+        var $quizNewQuestionAddToList = $('#quiz_newquestion_addtolist');
+        var $quizNewQuestionRaquoRight = $('#quiz_newquestion_raquo_right');
+        var quizNewQuestionCorrectComment = $('#quiz_newquestion_correctcomment');
+        var quizNewQuestionIncorrectComment = $('#quiz_newquestion_incorrectcomment');
+      	var $quizQuestionsListCreatedItems = $('#quiz_questionslist_createditems');
+        var $quizQuestionsListRemoveButton = $('.quiz_questionslist_remove_button');
+        var $quizQuestionsListDetailsButton = $('.quiz_questionslist_details_button');
+        var $quizContainerQuestionDetails = $('#quiz_container_questiondetails'); 
+        
+        //Class
+        var newQuestionRequired = '.newquestion_required';
+        var buttonDisabled = 's3d-disabled';
+        var quizQuestionsListSortableList = 'quiz_questionslist_sortablelist';
+        var cantBeDeleted = 'cant_be_deleted';
+        
+        //Id 
+        var quizNewQuestionTable = '#quiz_newquestion_table';
+        var quizNewQuestionQuestion = '#quiz_newquestion_question';
+        var quizNewQuestionAnswer_0 = '#quiz_newquestion_answer_0';
+        var quizNewQuestionAnswer_1 = '#quiz_newquestion_answer_1';
+        var quizQuestionsListItem = 'quiz_questionslist_item_';
+        
+        //Template 
+        var quizAddNewAnswerTableTemplate = 'quiz_add_newanswer_table_template';
+        var quizQuestionsListTemplate = 'quiz_questionslist_template';
+        var quizQuestionDetailsTemplate = 'quiz_questiondetails_template';
         
         
         /**
@@ -82,9 +97,9 @@ require(['jquery', 'sakai/sakai.api.core', 'jquery-ui'], function($, sakai) {
          * @return {Boolean} true if there is at least the question input and two answer inputs given by the user and if they are valid
          */
         var checkIfInputValid = function() {
-            var question = $(quizContainerAddNewQuestionQuestion).val();
-            var answer0 = $(quizContainerAddNewQuestionAnswer_0).val();
-            var answer1 = $(quizContainerAddNewQuestionAnswer_1).val();
+            var question = $(quizNewQuestionQuestion).val();
+            var answer0 = $($(quizNewQuestionAnswer_0).find('input[type=text]')[0]).val();
+            var answer1 = $($(quizNewQuestionAnswer_1).find('input[type=text]')[0]).val();
 
             // check if the user didn't just fill in some spaces
             return (question.replace(/ /g, "") !== "" && answer0.replace(/ /g, "") !== "" && answer1.replace(/ /g, "") !== "");
@@ -100,7 +115,7 @@ require(['jquery', 'sakai/sakai.api.core', 'jquery-ui'], function($, sakai) {
         	var answerInputs;
         	var answerTextInput;
         	var answerCheckboxInput = false;
-        	var rows = $(quizContainerAddNewQuestionTable).find('tr');
+        	var rows = $(quizNewQuestionTable).find('tr');
         	
         	questionsNumber++;
         	
@@ -119,71 +134,75 @@ require(['jquery', 'sakai/sakai.api.core', 'jquery-ui'], function($, sakai) {
         	}
         	
         	var contentQuestion = {
-        		"q" : $(quizContainerAddNewQuestionQuestion).val(),
+        		"q" : $(quizNewQuestionQuestion).val(),
         		"a" : answersTable,
-        		"correct" : $(quizContainerAddNewQuestionCorrectComment).val(),
-        		"incorrect" : $(quizContainerAddNewQuestionIncorrectComment).val()
+        		"correct" : $(quizNewQuestionCorrectComment).val(),
+        		"incorrect" : $(quizNewQuestionIncorrectComment).val()
         	};
         	questionsList.push(contentQuestion);
         };
 
         /** Binds all the regional settings select box change **/
-        $('.question_required', $quizContainerAddNewQuestion).keyup(function(e) {
+        $(newQuestionRequired, $quizNewQuestionForm).keyup(function(e) {
             // enable the save button
             if (checkIfInputValid()) {
-            	enableElements($($quizContainerAddNewQuestionToList));
-            	enableElements($($quizContainerAddNewQuestionRaquoRight));
+            	enableElements($($quizNewQuestionAddToList));
+            	enableElements($($quizNewQuestionRaquoRight));
             	questionCanBeSaved = true;
             }
             else {
            		if (questionCanBeSaved) {
-            		disableElements($($quizContainerAddNewQuestionToList));
-            		disableElements($($quizContainerAddNewQuestionRaquoRight));
+            		disableElements($($quizNewQuestionAddToList));
+            		disableElements($($quizNewQuestionRaquoRight));
             		questionCanBeSaved = false;
            	 	}
            	 }
         });
         
-        $quizContainerAddNewAnswerButton.on('click', function() {
+        $quizNewQuestionAddButton.on('click', function() {
         		answersNumber++;
         		var templateData = {
 	                'answersNumber': answersNumber
 	            };
-	            $quizContainerAddNewQuestionTable.append(
-                    sakai.api.Util.TemplateRenderer(quizAddNewQuestionTableTemplate, templateData)
+	            $quizNewQuestionTable.append(
+                    sakai.api.Util.TemplateRenderer(quizAddNewAnswerTableTemplate, templateData)
                 );
         });
         
-        $quizContainerRemoveLastAnswerButton.on('click', function() {
+        $quizNewQuestionRemoveButton.on('click', function() {
         		if (answersNumber > minAnswersNumber) {
         			answersNumber--;
-        			var rows = $(quizContainerAddNewQuestionTable).find('tr');
+        			var rows = $(quizNewQuestionTable).find('tr');
         			$(rows[rows.length-1]).remove();
         		}
         });
         
-       /* $quizContainerQuestionListShowDetails.live('click', function() {
+        $quizQuestionsListDetailsButton.live('click', function() {
         		var parentId = $(this).parent()[0].id;
-        		var index = parentId.split('quiz_container_question_to_add_')[1];
+        		var index = parentId.split(quizQuestionsListItem)[1];
         		var templateData = questionsList[index];
-        		$('#' + parentId).append(
-                    sakai.api.Util.TemplateRenderer(quizContainerQuestionDetailsTemplate, templateData)
+        		$quizContainerQuestionDetails.show();
+            	$quizContainerQuestionDetails.css('left', $(this).parents('li').position().left + 'px');
+            	$quizContainerQuestionDetails.css('top', $(this).parents('li').position().top + 40 + 'px');
+
+        		$quizContainerQuestionDetails.html(
+                    sakai.api.Util.TemplateRenderer(quizQuestionDetailsTemplate, templateData)
              	);
-        		
-        });*/
+        		return true;
+        });
         
-        $quizContainerAddNewQuestionRaquoRight.live('click', function() {
+        $quizNewQuestionRaquoRight.live('click', function() {
         	addQuestionToList();
         	resetQuestionInputs();
         });
         
-        $quizContainerAddNewQuestionToList.on('click', function() {
+        $quizNewQuestionAddToList.on('click', function() {
         	addQuestionToList();
         	resetQuestionInputs();
         });
         
-        $quizContainerQuestionToRemove.live('click', function(){
-        	var index = $(this).parent()[0].id.split('quiz_container_question_to_add_')[1];
+        $quizQuestionsListRemoveButton.live('click', function(){
+        	var index = $(this).parent()[0].id.split(quizQuestionsListItem)[1];
         	questionsList.splice(index,1);
         	questionsNumber--;
         	renderQuestionsList();
@@ -201,12 +220,12 @@ require(['jquery', 'sakai/sakai.api.core', 'jquery-ui'], function($, sakai) {
         		'questions': questionsList
 			};
 		
-        	$quizContainerQuestionListUl.html(
-                    sakai.api.Util.TemplateRenderer(quizContainerQuestionListTemplate, templateData)
+        	$quizQuestionsListCreatedItems.html(
+                    sakai.api.Util.TemplateRenderer(quizQuestionsListTemplate, templateData)
              );
              
-            $(".sortable" ).sortable({
-            	connectWith: '.sortable',
+            $(quizQuestionsListSortableList).sortable({
+            	connectWith: quizQuestionsListSortableList,
                 revert: true,
                 opacity: 0.5,
                 tolerance: 'intersect'
@@ -214,20 +233,20 @@ require(['jquery', 'sakai/sakai.api.core', 'jquery-ui'], function($, sakai) {
         }
         
         var resetQuestionInputs = function() {
-        	$(quizContainerAddNewQuestionQuestion).val('');
-        	$(quizContainerAddNewQuestionCorrectComment).val('');
-        	$(quizContainerAddNewQuestionIncorrectComment).val('');
-        	$(quizContainerAddNewQuestionAnswer_0).val('');
-        	$(quizContainerAddNewQuestionAnswer_1).val('');
+        	$(quizNewQuestionQuestion).val('');
+        	$(quizNewQuestionCorrectComment).val('');
+        	$(quizNewQuestionIncorrectComment).val('');
         	
-        	disableElements($($quizContainerAddNewQuestionToList));
-        	disableElements($($quizContainerAddNewQuestionRaquoRight));
+        	disableElements($($quizNewQuestionAddToList));
+        	disableElements($($quizNewQuestionRaquoRight));
             questionCanBeSaved = false;
-        	$(quizContainerAddNewQuestionTable).find('tr').each(function() {
-        		if ($(this).attr('class') != 'cant_be_deleted') {
+        	$(quizNewQuestionTable).find('tr').each(function() {
+        		if ($(this).attr('class') != cantBeDeleted) {
         			$(this).remove();
         		}
         		else {
+            		$($(quizNewQuestionAnswer_0).find('input[type=text]')[0]).val('');
+           			$($(quizNewQuestionAnswer_1).find('input[type=text]')[0]).val('');
         			$($(this).find('input[type=checkbox]')[0]).attr('checked', false);
         		}
         	});
